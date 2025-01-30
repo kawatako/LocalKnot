7135effa3b0095b981b9c4d208d8b13b0658c7b8
kawatashota <qhqn_hqnd@icloud.com>
2025-01-30 15:32:05 +0900

    Remove solid_queue configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,8 +50,8 @@ Rails.application.configure do
   config.cache_store = :solid_cache_store
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
-  config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  # config.active_job.queue_adapter = :solid_queue
+  # config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "example.com" }
